332d3496982be7897652659e4ab26646838ab449
Julian <julian@Administrators-MacBook-Pro.local>
2023-08-29 09:45:19 -0400

    quiz modifications

---
+++ src/components/QuizData.js
@@ -0,0 +1,38 @@
+export const questions = [
+    {
+        question: "What is a good spending habit?",
+        answers: [
+            { text: "Buying whatever you want without thinking", correct: false },
+            { text: "Creating a budget and tracking your expenses", correct: true },
+            { text: "Maxing out credit cards for luxury items", correct: false },
+            { text: "Spending without saving any money", correct: false },
+        ],
+    },
+    {
+        question: "Which of the following is a wise financial decision?",
+        answers: [
+            { text: "Taking out a high-interest payday loan", correct: false },
+            { text: "Investing in a diversified portfolio", correct: true },
+            { text: "Splurging on expensive vacations every month", correct: false },
+            { text: "Paying only the minimum balance on credit cards", correct: false },
+        ],
+    },
+    {
+        question: "What should you do before making a big purchase?",
+        answers: [
+            { text: "Avoid checking your bank account", correct: false },
+            { text: "Research the product and compare prices", correct: true },
+            { text: "Assume you can always return it if you don't like it", correct: false },
+            { text: "Buy it impulsively to avoid missing out", correct: false },
+        ],
+    },
+    {
+        question: "What is an emergency fund used for?",
+        answers: [
+            { text: "Buying luxury items", correct: false },
+            { text: "Paying off all debts at once", correct: false },
+            { text: "Covering unexpected expenses and emergencies", correct: true },
+            { text: "Funding extravagant parties", correct: false },
+        ],
+    },
+];
